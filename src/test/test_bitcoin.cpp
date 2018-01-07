tmagik/catoshi

test: Report InitBlockIndex result
If InitBlockIndex fails, then it will segfault later. Same for the later
ActivateBestChain. BOOST_REQUIRE the result, so that an error will be
reported and the test case aborted.

src/test/test_bitcoin.cpp
@@ -69,11 +69,11 @@ TestingSetup::TestingSetup(const std::string& chainName) : BasicTestingSetup(cha
         pblocktree = new CBlockTreeDB(1 << 20, true);
         pcoinsdbview = new CCoinsViewDB(1 << 23, true);
         pcoinsTip = new CCoinsViewCache(pcoinsdbview);
-        InitBlockIndex(chainparams);
+        BOOST_REQUIRE(InitBlockIndex(chainparams));
         {
             CValidationState state;
             bool ok = ActivateBestChain(state, chainparams);
-            BOOST_CHECK(ok);
+            BOOST_REQUIRE(ok);
         }
         nScriptCheckThreads = 3;
         for (int i=0; i < nScriptCheckThreads-1; i++)

TuringBitChain/TBCNODE

Summary:
Backport of core's PR9904
Test Plan:
  make check
Reviewers: schancel, #bitcoin_abc, dagurval
Reviewed By: #bitcoin_abc, dagurval
Differential Revision: https://reviews.bitcoinabc.org/D903

src/test/test_bitcoin.cpp
@@ -88,11 +88,11 @@ TestingSetup::TestingSetup(const std::string &chainName)
     pblocktree = new CBlockTreeDB(1 << 20, true);
     pcoinsdbview = new CCoinsViewDB(1 << 23, true);
     pcoinsTip = new CCoinsViewCache(pcoinsdbview);
-    InitBlockIndex(config);
+    BOOST_REQUIRE(InitBlockIndex(config));
     {
         CValidationState state;
         bool ok = ActivateBestChain(config, state);
-        BOOST_CHECK(ok);
+        BOOST_REQUIRE(ok);
     }
     nScriptCheckThreads = 3;
     for (int i = 0; i < nScriptCheckThreads - 1; i++) {