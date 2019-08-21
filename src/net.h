TuringBitChain/TBCNODE

commit 548c7fb41c37d6856499cb49776fdd7a462042b1

Make use of threadpool to do some processing in parallel.

src/net.h
@@ -22,6 +22,7 @@
 #include "random.h"
 #include "streams.h"
 #include "sync.h"
+#include "task_helpers.h"
 #include "threadinterrupt.h"
 #include "txmempool.h"
 #include "txn_sending_details.h"
@@ -182,36 +183,31 @@ public:
     /** Get a handle to our transaction propagator */
     const std::shared_ptr<CTxnPropagator>& getTransactionPropagator() const { return mTxnPropagator; }
 
-    template <typename Callable> void ForEachNode(Callable &&func) {
+    /** Call the specified function for each node */
+    template <typename Callable> void ForEachNode(Callable&& func) const {
         LOCK(cs_vNodes);
-        for (auto &&node : vNodes) {
-            if (NodeFullyConnected(node)) func(node);
+        for(const CNodePtr& node : vNodes) {
+            if(NodeFullyConnected(node))
+                func(node);
         }
     };
 
-    template <typename Callable> void ForEachNode(Callable &&func) const {
-        LOCK(cs_vNodes);
-        for (auto &&node : vNodes) {
-            if (NodeFullyConnected(node)) func(node);
-        }
-    };
+    /** Call the specified function for each node in parallel */
+    template <typename Callable>
+    auto ParallelForEachNode(Callable&& func)
+        -> std::vector<std::future<typename std::result_of<Callable(const CNodePtr&)>::type>>
+    {
+        using resultType = typename std::result_of<Callable(const CNodePtr&)>::type;
+        std::vector<std::future<resultType>> results {};
+        results.reserve(vNodes.size());
 
-    template <typename Callable, typename CallableAfter>
-    void ForEachNodeThen(Callable &&pre, CallableAfter &&post) {
         LOCK(cs_vNodes);
-        for (auto &&node : vNodes) {
-            if (NodeFullyConnected(node)) pre(node);
+        for(const CNodePtr& node : vNodes) {
+            if(NodeFullyConnected(node))
+                results.emplace_back(make_task(mThreadPool, func, node));
         }
-        post();
-    };
 
-    template <typename Callable, typename CallableAfter>
-    void ForEachNodeThen(Callable &&pre, CallableAfter &&post) const {
-        LOCK(cs_vNodes);
-        for (auto &&node : vNodes) {
-            if (NodeFullyConnected(node)) pre(node);
-        }
-        post();
+        return results;
     };
 
     // Addrman functions
@@ -415,6 +411,8 @@ private:
 
     /** Transaction tracker/propagator */
     std::shared_ptr<CTxnPropagator> mTxnPropagator {};
+
+    CThreadPool<CQueueAdaptor> mThreadPool { "ConnmanPool" };
 
     CThreadInterrupt interruptNet;
 
@@ -674,7 +672,6 @@ public:
     std::vector<CAddress> vAddrToSend;
     CRollingBloomFilter addrKnown;
     bool fGetAddr;
-    std::set<uint256> setKnown;
     int64_t nNextAddrSend;
     int64_t nNextLocalAddrSend;
 

src/net_processing.cpp
@@ -1127,8 +1127,7 @@ static void RelayAddress(const CAddress &addr, bool fReachable,
             .Write((GetTime() + hashAddr) / (24 * 60 * 60));
     FastRandomContext insecure_rand;
 
-    std::array<std::pair<uint64_t, CNodePtr>, 2> best{
-        {{0, nullptr}, {0, nullptr}}};
+    std::array<std::pair<uint64_t, CNodePtr>, 2> best {{{0, nullptr}, {0, nullptr}}};
     assert(nRelayNodes <= best.size());
 
     auto sortfunc = [&best, &hasher, nRelayNodes](const CNodePtr& pnode) {
@@ -1144,14 +1143,11 @@ static void RelayAddress(const CAddress &addr, bool fReachable,
             }
         }
     };
+    connman.ForEachNode(sortfunc);
 
-    auto pushfunc = [&addr, &best, nRelayNodes, &insecure_rand] {
-        for (unsigned int i = 0; i < nRelayNodes && best[i].first != 0; i++) {
-            best[i].second->PushAddress(addr, insecure_rand);
-        }
-    };
-
-    connman.ForEachNodeThen(std::move(sortfunc), std::move(pushfunc));
+    for (unsigned int i = 0; i < nRelayNodes && best[i].first != 0; i++) {
+        best[i].second->PushAddress(addr, insecure_rand);
+    }
 }
 
 static void ProcessGetData(const Config &config, const CNodePtr& pfrom,

src/threadpoolT.h
@@ -40,7 +40,7 @@ CThreadPool<QueueAdaptor>::~CThreadPool()
 template<typename QueueAdaptor>
 void CThreadPool<QueueAdaptor>::worker(size_t n)
 {
-    LogPrintf("%s ThreadPool thread %d starting", mOwnerStr.c_str(), n);
+    LogPrintf("%s ThreadPool thread %d starting\n", mOwnerStr.c_str(), n);
 
     while(mRunning)
     {
@@ -64,7 +64,7 @@ void CThreadPool<QueueAdaptor>::worker(size_t n)
         task();
     }
 
-    LogPrintf("%s ThreadPool thread %d stopping", mOwnerStr.c_str(), n);
+    LogPrintf("%s ThreadPool thread %d stopping\n", mOwnerStr.c_str(), n);
 }
 
 // Submit a task to the pool.

src/txn_propagator.cpp
@@ -94,7 +94,11 @@ void CTxnPropagator::removeTransactions(const std::vector<CTransactionRef>& txns
     // Update lists of pending transactions for each node
     {
         LOCK(mempool.cs);
-        g_connman->ForEachNode([&txnDetails](const CNodePtr& node) { node->RemoveTxnsFromInventory(txnDetails); });
+        auto results { g_connman->ParallelForEachNode([&txnDetails](const CNodePtr& node) { node->RemoveTxnsFromInventory(txnDetails); }) };
+
+        // Wait for all nodes to finish processing so we can safely release the mempool lock
+        for(auto& result : results)
+            result.wait();
     }
 }
 
@@ -152,7 +156,11 @@ void CTxnPropagator::processNewTransactions()
     {
         // Take the mempool lock so we can do all the difficult txn sorting and node updating in parallel.
         LOCK(mempool.cs);
-        g_connman->ForEachNode([this](const CNodePtr& node) { node->AddTxnsToInventory(mNewTxns); });
+        auto results { g_connman->ParallelForEachNode([this](const CNodePtr& node) { node->AddTxnsToInventory(mNewTxns); }) };
+
+        // Wait for all nodes to finish processing so we can safely release the mempool lock
+        for(auto& result : results)
+            result.wait();
     }
 
     // Clear new transactions list

commit 14646ac76d6f22bce5774f7a3acdf9b63ff4bc1e

vNodes in ParallelForEachNode was no longer thread safe
commit 548c7fb41c37d6856499cb49776fdd7a462042b1 introduced
a race condition when accessing vNodes member variable

src/net.h
@@ -198,9 +198,9 @@ public:
     {
         using resultType = typename std::result_of<Callable(const CNodePtr&)>::type;
         std::vector<std::future<resultType>> results {};
-        results.reserve(vNodes.size());
 
         LOCK(cs_vNodes);
+        results.reserve(vNodes.size());
         for(const CNodePtr& node : vNodes) {
             if(NodeFullyConnected(node))
                 results.emplace_back(make_task(mThreadPool, func, node));