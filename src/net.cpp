TuringBitChain/TBCNODE

commit 584a5d9aae39ce3cd77a50afc4600ea5d169bcc6

Debug log duration for p2p request processing that takes too long
debugp2pthreadstalls parameter can be set to log p2p requests
that take longer than the provided value in milliseconds.
This enables us to detect p2p requests that block other request
processing for longer than we would like.

src/net.cpp
@@ -38,6 +38,7 @@
 #endif
 
 #include <cmath>
+#include <optional>
 #include <utility>
 
 // Dump addresses to peers.dat and banlist.dat every 15 minutes (900s)
@@ -2159,6 +2160,52 @@ bool CConnman::OpenNetworkConnection(const CAddress &addrConnect,
     return true;
 }
 
+namespace
+{
+    /**
+     * Helper class for logging the duration of ThreadMessageHandler reqest
+     * processing. It writes to log all the requests that take more time to
+     * process than the provided threshold.
+     */
+    class CLogP2PStallDuration
+    {
+    public:
+        CLogP2PStallDuration(
+            std::string command,
+            std::chrono::milliseconds debugP2PTheadStallsThreshold)
+            : mDebugP2PTheadStallsThreshold{debugP2PTheadStallsThreshold}
+            , mProcessingStart{std::chrono::steady_clock::now()}
+            , mCommand{std::move(command)}
+        {/**/}
+
+
+        ~CLogP2PStallDuration()
+        {
+            if(!mCommand.empty())
+            {
+                auto processingDuration =
+                        std::chrono::steady_clock::now() - mProcessingStart;
+
+                if(processingDuration > mDebugP2PTheadStallsThreshold)
+                {
+                    LogPrint(
+                        BCLog::NET,
+                        "CConnman request processing took %s ms to complete "
+                        "processing '%s' request!\n",
+                        std::chrono::duration_cast<std::chrono::milliseconds>(
+                            processingDuration).count(),
+                        mCommand);
+                }
+            }
+        }
+
+    private:
+        std::chrono::milliseconds mDebugP2PTheadStallsThreshold;
+        std::chrono::time_point<std::chrono::steady_clock> mProcessingStart;
+        std::string mCommand;
+    };
+}
+
 void CConnman::ThreadMessageHandler() {
     while (!flagInterruptMsgProc) {
         std::vector<CNodePtr> vNodesCopy;
@@ -2172,6 +2219,20 @@ void CConnman::ThreadMessageHandler() {
         for (const CNodePtr& pnode : vNodesCopy) {
             if (pnode->fDisconnect) {
                 continue;
+            }
+
+            std::optional<CLogP2PStallDuration> durationLog;
+
+            using namespace std::literals::chrono_literals;
+
+            if(!pnode->vProcessMsg.empty() &&
+                mDebugP2PTheadStallsThreshold > 0ms)
+            {
+                durationLog =
+                    {
+                        pnode->vProcessMsg.begin()->hdr.GetCommand(),
+                        mDebugP2PTheadStallsThreshold
+                    };
             }
 
             // Receive messages
@@ -2188,6 +2249,7 @@ void CConnman::ThreadMessageHandler() {
                 GetNodeSignals().SendMessages(*config, pnode, *this,
                                               flagInterruptMsgProc);
             }
+
             if (flagInterruptMsgProc) {
                 return;
             }
@@ -2391,8 +2453,16 @@ void CConnman::SetNetworkActive(bool active) {
     uiInterface.NotifyNetworkActiveChanged(fNetworkActive);
 }
 
-CConnman::CConnman(const Config &configIn, uint64_t nSeed0In, uint64_t nSeed1In)
-    : config(&configIn), nSeed0(nSeed0In), nSeed1(nSeed1In) {
+CConnman::CConnman(
+    const Config &configIn,
+    uint64_t nSeed0In,
+    uint64_t nSeed1In,
+    std::chrono::milliseconds debugP2PTheadStallsThreshold)
+    : config(&configIn)
+    , nSeed0(nSeed0In)
+    , nSeed1(nSeed1In)
+    , mDebugP2PTheadStallsThreshold{debugP2PTheadStallsThreshold}
+{
     fNetworkActive = true;
     setBannedIsDirty = false;
     fAddressesInitialized = false;

src/init.cpp
@@ -55,6 +55,7 @@
 #include <cstdio>
 #include <memory>
 #include <cinttypes>
+#include <chrono>
 
 #ifndef WIN32
 #include <signal.h>
@@ -748,6 +749,10 @@ std::string HelpMessage(HelpMessageMode mode) {
     strUsage += HelpMessageOpt(
         "-help-debug",
         _("Show all debugging options (usage: --help -help-debug)"));
+    strUsage += HelpMessageOpt(
+        "-debugp2pthreadstalls",
+        _("Log P2P requests that stall request processing loop for longer than "
+          "specified milliseconds (default: disabled)"));
     strUsage += HelpMessageOpt(
         "-logips",
         strprintf(_("Include IP addresses in debug output (default: %d)"),
@@ -1934,9 +1939,14 @@ bool AppInitMain(Config &config, boost::thread_group &threadGroup,
     // need to reindex later.
 
     assert(!g_connman);
-    g_connman = std::unique_ptr<CConnman>(
-        new CConnman(config, GetRand(std::numeric_limits<uint64_t>::max()),
-                     GetRand(std::numeric_limits<uint64_t>::max())));
+    {
+        int64_t duration = gArgs.GetArg("-debugp2pthreadstalls", 0);
+        g_connman =
+            std::make_unique<CConnman>(
+                config, GetRand(std::numeric_limits<uint64_t>::max()),
+                GetRand(std::numeric_limits<uint64_t>::max()),
+                std::chrono::milliseconds{duration > 0 ? duration : 0});
+    }
     CConnman &connman = *g_connman;
 
     peerLogic.reset(new PeerLogicValidation(&connman));

src/net.h
@@ -31,6 +31,7 @@
 
 #include <atomic>
 #include <condition_variable>
+#include <chrono>
 #include <cstdint>
 #include <deque>
 #include <memory>
@@ -183,7 +184,11 @@ public:
         uint64_t nMaxOutboundTimeframe = 0;
         uint64_t nMaxOutboundLimit = 0;
     };
-    CConnman(const Config &configIn, uint64_t seed0, uint64_t seed1);
+    CConnman(
+        const Config &configIn,
+        uint64_t seed0,
+        uint64_t seed1,
+        std::chrono::milliseconds debugP2PTheadStallsThreshold);
     ~CConnman();
     bool Start(CScheduler &scheduler, std::string &strNodeError,
                Options options);
@@ -450,6 +455,8 @@ private:
     std::thread threadOpenAddedConnections;
     std::thread threadOpenConnections;
     std::thread threadMessageHandler;
+
+    std::chrono::milliseconds mDebugP2PTheadStallsThreshold;
 };
 extern std::unique_ptr<CConnman> g_connman;
 void Discover(boost::thread_group &threadGroup);

src/test/test_bitcoin.cpp
@@ -100,7 +100,9 @@ TestingSetup::TestingSetup(const std::string &chainName)
     }
 
     // Deterministic randomness for tests.
-    g_connman = std::unique_ptr<CConnman>(new CConnman(config, 0x1337, 0x1337));
+    g_connman =
+        std::make_unique<CConnman>(
+            config, 0x1337, 0x1337, std::chrono::milliseconds{0});
     connman = g_connman.get();
     RegisterNodeSignals(GetNodeSignals());
 }

commit b9be7bd79ab2a4c4f8b3ba82f93917447376a48d

Race condition in p2p node messages access
Commit 584a5d9aae39ce3cd77a50afc4600ea5d169bcc6 introduced
pnode->vProcessMsg access race condition.

src/net.cpp
@@ -2225,14 +2225,17 @@ void CConnman::ThreadMessageHandler() {
 
             using namespace std::literals::chrono_literals;
 
-            if(!pnode->vProcessMsg.empty() &&
-                mDebugP2PTheadStallsThreshold > 0ms)
+            if(mDebugP2PTheadStallsThreshold > 0ms)
             {
-                durationLog =
-                    {
-                        pnode->vProcessMsg.begin()->hdr.GetCommand(),
-                        mDebugP2PTheadStallsThreshold
-                    };
+                LOCK(pnode->cs_vProcessMsg);
+                if(!pnode->vProcessMsg.empty())
+                {
+                    durationLog =
+                        {
+                            pnode->vProcessMsg.begin()->hdr.GetCommand(),
+                            mDebugP2PTheadStallsThreshold
+                        };
+                }
             }
 
             // Receive messages