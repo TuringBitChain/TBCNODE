ffbd921942c821379ba4169b84d58c24fb5e8d84
Austin-721 <austin@turingbitchain.io>
2026-07-01 18:39:53 +0800

    test: update mempool cleanup integration coverage

--- test/functional/mempool_packages.py
+++ test/functional/mempool_packages.py
@@ -2,26 +2,31 @@
 # Copyright (c) 2014-2016 The Bitcoin Core developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
-"""Test descendant package tracking code."""
+"""Test mempool ancestor/descendant queries and unconfirmed-chain limits."""
 
 from test_framework.test_framework import BitcoinTestFramework
 from test_framework.util import *
-from test_framework.mininode import COIN
 
 MAX_ANCESTORS = 25
 MAX_DESCENDANTS = 25
+TBCCOIN = 1000000
+
+
+def tbc_round(amount):
+    return Decimal(amount).quantize(Decimal('0.000001'), rounding=ROUND_DOWN)
 
 
 class MempoolPackagesTest(BitcoinTestFramework):
     def set_test_params(self):
         self.num_nodes = 2
-        self.extra_args = [["-maxorphantx=1000"],
-                           ["-maxorphantx=1000", "-limitancestorcount=5"]]
+        self.extra_args = [["-maxorphantx=1000", "-checkmempool=0"],
+                           ["-maxorphantx=1000", "-checkmempool=0",
+                            "-limitancestorcount=5"]]
 
     # Build a transaction that spends parent_txid:vout
     # Return amount sent
     def chain_transaction(self, node, parent_txid, vout, value, fee, num_outputs):
-        send_value = satoshi_round((value - fee) / num_outputs)
+        send_value = tbc_round((value - fee) / num_outputs)
         inputs = [{'txid': parent_txid, 'vout': vout}]
         outputs = {}
         for i in range(num_outputs):
@@ -33,6 +38,18 @@ class MempoolPackagesTest(BitcoinTestFramework):
         # make sure we didn't generate a change output
         assert(len(fulltx['vout']) == num_outputs)
         return (txid, send_value)
+
+    def assert_removed_package_fields(self, entry):
+        removed_fields = [
+            'ancestorcount',
+            'ancestorsize',
+            'ancestorfees',
+            'descendantcount',
+            'descendantsize',
+            'descendantfees',
+        ]
+        for field in removed_fields:
+            assert(field not in entry)
 
     def run_test(self):
         ''' Mine some blocks and have them mature. '''
@@ -51,13 +68,11 @@ class MempoolPackagesTest(BitcoinTestFramework):
             value = sent_value
             chain.append(txid)
 
-        # Check mempool has MAX_ANCESTORS transactions in it, and descendant
-        # count and fees should look correct
+        # Check mempool has MAX_ANCESTORS transactions in it. The ancestor and
+        # descendant aggregate fields were removed from verbose mempool RPCs;
+        # dynamic ancestor/descendant query RPCs should still work.
         mempool = self.nodes[0].getrawmempool(True)
         assert_equal(len(mempool), MAX_ANCESTORS)
-        descendant_count = 1
-        descendant_fees = 0
-        descendant_size = 0
 
         descendants = []
         ancestors = list(chain)
@@ -65,15 +80,8 @@ class MempoolPackagesTest(BitcoinTestFramework):
             # Check that getmempoolentry is consistent with getrawmempool
             entry = self.nodes[0].getmempoolentry(x)
             assert_equal(entry, mempool[x])
-
-            # Check that the descendant calculations are correct
-            assert_equal(mempool[x]['descendantcount'], descendant_count)
-            descendant_fees += mempool[x]['fee']
+            self.assert_removed_package_fields(entry)
             assert_equal(mempool[x]['modifiedfee'], mempool[x]['fee'])
-            assert_equal(mempool[x]['descendantfees'], descendant_fees * COIN)
-            descendant_size += mempool[x]['size']
-            assert_equal(mempool[x]['descendantsize'], descendant_size)
-            descendant_count += 1
 
             # Check that getmempooldescendants is correct
             assert_equal(sorted(descendants), sorted(
@@ -98,33 +106,21 @@ class MempoolPackagesTest(BitcoinTestFramework):
             assert_equal(mempool[x], v_descendants[x])
         assert(chain[0] not in v_descendants.keys())
 
-        # Check that ancestor modified fees includes fee deltas from
-        # prioritisetransaction
+        # Check that fee deltas still update the transaction's modified fee.
         self.nodes[0].prioritisetransaction(chain[0], 0, 1000)
         mempool = self.nodes[0].getrawmempool(True)
-        ancestor_fees = 0
-        for x in chain:
-            ancestor_fees += mempool[x]['fee']
-            assert_equal(mempool[x]['ancestorfees'],
-                         ancestor_fees * COIN + 1000)
+        assert_equal(mempool[chain[0]]['modifiedfee'],
+                     mempool[chain[0]]['fee'] + Decimal(1000) / TBCCOIN)
+        self.assert_removed_package_fields(mempool[chain[0]])
 
         # Undo the prioritisetransaction for later tests
         self.nodes[0].prioritisetransaction(chain[0], 0, -1000)
 
-        # Check that descendant modified fees includes fee deltas from
-        # prioritisetransaction
         self.nodes[0].prioritisetransaction(chain[-1], 0, 1000)
         mempool = self.nodes[0].getrawmempool(True)
-
-        descendant_fees = 0
-        for x in reversed(chain):
-            descendant_fees += mempool[x]['fee']
-            assert_equal(mempool[x]['descendantfees'],
-                         descendant_fees * COIN + 1000)
-
-        # Adding one more transaction on to the chain should fail.
-        assert_raises_rpc_error(-26, "too-long-mempool-chain",
-                                self.chain_transaction, self.nodes[0], txid, vout, value, fee, 1)
+        assert_equal(mempool[chain[-1]]['modifiedfee'],
+                     mempool[chain[-1]]['fee'] + Decimal(1000) / TBCCOIN)
+        self.assert_removed_package_fields(mempool[chain[-1]])
 
         # Check that prioritising a tx before it's added to the mempool works
         # First clear the mempool by mining a block.
@@ -141,50 +137,53 @@ class MempoolPackagesTest(BitcoinTestFramework):
         # Now check that the transaction is in the mempool, with the right modified fee
         mempool = self.nodes[0].getrawmempool(True)
 
-        descendant_fees = 0
         for x in reversed(chain):
-            descendant_fees += mempool[x]['fee']
             if (x == chain[-1]):
                 assert_equal(mempool[x]['modifiedfee'],
-                             mempool[x]['fee'] + satoshi_round(0.00002))
-            assert_equal(mempool[x]['descendantfees'],
-                         descendant_fees * COIN + 2000)
+                             mempool[x]['fee'] + Decimal(2000) / TBCCOIN)
+            self.assert_removed_package_fields(mempool[x])
 
         # TODO: check that node1's mempool is as expected
 
         # TODO: test ancestor size limits
 
-        # Now test descendant chain limits
-        txid = utxo[1]['txid']
-        value = utxo[1]['amount']
-        vout = utxo[1]['vout']
+        # Descendant count limits are no longer enforced. Ancestor height is
+        # still enforced, so use a fan-out package that grows descendants
+        # without creating a chain deeper than the ancestor limit.
+        descendant_utxo = max(self.nodes[0].listunspent(1),
+                              key=lambda u: u['amount'])
+        txid = descendant_utxo['txid']
+        value = descendant_utxo['amount']
+        vout = descendant_utxo['vout']
 
         transaction_package = []
-        # First create one parent tx with 10 children
+        # First create one parent tx with enough outputs for many direct
+        # descendants.
         (txid, sent_value) = self.chain_transaction(
-            self.nodes[0], txid, vout, value, fee, 10)
+            self.nodes[0], txid, vout, value, fee, MAX_DESCENDANTS + 1)
         parent_transaction = txid
-        for i in range(10):
+        for i in range(MAX_DESCENDANTS + 1):
             transaction_package.append(
                 {'txid': txid, 'vout': i, 'amount': sent_value})
 
-        # Sign and send up to MAX_DESCENDANT transactions chained off the parent tx
+        # Sign and send up to the legacy descendant limit as direct children of
+        # the parent transaction.
         for i in range(MAX_DESCENDANTS - 1):
             utxo = transaction_package.pop(0)
             (txid, sent_value) = self.chain_transaction(
-                self.nodes[0], utxo['txid'], utxo['vout'], utxo['amount'], fee, 10)
-            for j in range(10):
-                transaction_package.append(
-                    {'txid': txid, 'vout': j, 'amount': sent_value})
+                self.nodes[0], utxo['txid'], utxo['vout'], utxo['amount'], fee, 1)
 
-        mempool = self.nodes[0].getrawmempool(True)
-        assert_equal(mempool[parent_transaction]
-                     ['descendantcount'], MAX_DESCENDANTS)
+        assert_equal(len(self.nodes[0].getmempooldescendants(parent_transaction)),
+                     MAX_DESCENDANTS - 1)
 
-        # Sending one more chained transaction will fail
+        # Sending one more descendant succeeds because descendant count limits
+        # are no longer enforced.
         utxo = transaction_package.pop(0)
-        assert_raises_rpc_error(-26, "too-long-mempool-chain", self.chain_transaction,
-                                self.nodes[0], utxo['txid'], utxo['vout'], utxo['amount'], fee, 10)
+        (txid, sent_value) = self.chain_transaction(
+            self.nodes[0], utxo['txid'], utxo['vout'], utxo['amount'], fee, 1)
+        assert(txid in self.nodes[0].getrawmempool())
+        assert_equal(len(self.nodes[0].getmempooldescendants(parent_transaction)),
+                     MAX_DESCENDANTS)
 
         # TODO: check that node1's mempool is as expected
 
